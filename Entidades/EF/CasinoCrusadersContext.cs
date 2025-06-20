68f9e0eef0bcd4013ffd6be10ed793165479e99c
Martu <143520216+MartuPe@users.noreply.github.com>
2025-06-20 15:05:09 -0300

    fondo nuevo y arreglos en el header

--- Entidades/EF/CasinoCrusadersContext.cs
+++ Entidades/EF/CasinoCrusadersContext.cs
@@ -31,7 +31,7 @@ public partial class CasinoCrusadersContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-JHC0DJ4\\SQLEXPRESS;Database=CasinoCrusaders;Trusted_Connection=True;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-BDT1HJL\\SQLEXPRESS;Database=CasinoCrusaders;Trusted_Connection=True;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
